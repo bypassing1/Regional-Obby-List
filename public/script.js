4e35f85477d5a2be017c831f50ac864817e2d37a
crafted <160091348+bypassing1@users.noreply.github.com>
2024-08-06 18:02:11 +0700

    Vercel Fix

--- public/script.js
+++ public/script.js
@@ -1,13 +1,3 @@
-document.body.innerHTML += `
-    <script>
-        window.si = window.si || function () { (window.siq = window.siq || []).push(arguments); };
-    </script>
-    <script defer src="/_vercel/speed-insights/script.js"></script>
-    <script>
-    window.va = window.va || function () { (window.vaq = window.vaq || []).push(arguments); };
-    </script>
-    <script defer src="/_vercel/insights/script.js"></script>
-    `
 document.addEventListener('DOMContentLoaded', function () {
     // Fetch the JSON data
     // Fetch globaldata.json and playerstat.json
