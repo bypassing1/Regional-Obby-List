622f527e74cf29bc840f82a54642b4bacfddc387
crafted <160091348+bypassing1@users.noreply.github.com>
2024-08-06 17:44:17 +0700

    Vercel Update + Traffic Fix

--- public/script.js
+++ public/script.js
@@ -1,3 +1,13 @@
+document.body.innerHTML += `
+    <script>
+        window.si = window.si || function () { (window.siq = window.siq || []).push(arguments); };
+    </script>
+    <script defer src="/_vercel/speed-insights/script.js"></script>
+    <script>
+    window.va = window.va || function () { (window.vaq = window.vaq || []).push(arguments); };
+    </script>
+    <script defer src="/_vercel/insights/script.js"></script>
+    `
 document.addEventListener('DOMContentLoaded', function () {
     // Fetch the JSON data
     // Fetch globaldata.json and playerstat.json
