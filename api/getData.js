2e8c48dc74e567518f3fabd5b4b36d8b8ffcd309
crafted <zylenox12@gmail.com>
2025-04-11 16:15:30 +0700

    s

--- api/getData.js
+++ api/getData.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY);
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_KEY);
 
 export default async function handler(req, res) {
     if (req.method !== 'GET') {
